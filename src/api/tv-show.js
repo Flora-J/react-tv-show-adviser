a44638f06c2fdc6fc6d83339805aaffdf3036e87
Flora JULIE <flora.julie@icloud.com>
2023-10-18 15:09:43 +0200

    useEffect

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,12 +1,9 @@
 import axios from "axios";
-
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=eaaef303d8e9b7ecad56db3d6303b298";
+import { BASE_URL, API_KEY_PARAM } from "../config";
 
 export class TVShowAPI {
   static async fetchPopulars() {
     const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-    console.log("***", response.data.results);
     return response.data.results;
   }
 }
